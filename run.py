91a21f14c7d8a5195535619797181ece2aedefb9
sherryrich <ricsherry@gmail.com>
2022-04-27 10:12:48 +0000

    Retrieve latest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -76,7 +76,7 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    pprint(stock)
+    print(stock_row)
 
 def main():
     """
